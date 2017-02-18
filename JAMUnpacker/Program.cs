600037552f0da62de52d9b6c7f74318864b4f40c
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 18:55:10 -0500

    Added error handling involving file access

--- JAMUnpacker/Program.cs
+++ JAMUnpacker/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,25 @@ namespace JAMUnpacker
             if(args.Length != 1)
             {
                 Console.WriteLine("JAMUnpacker\nUnpacks JAM files used by the relatively unknown game, The Grim Adventures of Billy and Mandy.\n\nUsage: JAMUnpacker <jamfile.jam>");
+                return;
+            }
+            try
+            {
+                FileStream savegameStream = new FileStream(args[0], FileMode.Open, FileAccess.ReadWrite);
+
+                // Create our binary reader
+                BinaryReader reader = new BinaryReader((Stream)savegameStream);
+            }
+
+            // Bit of error handling for when the file is either not there or unaccessible
+            catch (FileNotFoundException)
+            {
+                System.Console.WriteLine("The JAM you are trying to unpack can't be found.");
+                return;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                System.Console.WriteLine("The JAM you are trying to unpack could not be unpacked because access was denied.");
             }
         }
     }
