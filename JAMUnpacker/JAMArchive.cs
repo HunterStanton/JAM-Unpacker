65976fcd2e4bff13f31646b5813d2a703ad83a9c
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 18:46:43 -0500

    Added JAM struct stub

---
+++ JAMUnpacker/JAMArchive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMUnpacker
+{
+    /// <summary>
+    /// The structure of the JAM file used by the game.
+    /// </summary>
+    class JAMArchive
+    {
+    }
+}
